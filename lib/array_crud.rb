cd5ce524cb8a69d2aa7f98617fbf1c6e7ed5d5f5
John Michael Robey <robey14@gmail.com>
2020-01-27 01:23:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift ("six")
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
@@ -25,7 +25,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[3]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
